af9e855cc70d04563b142e89faa6eb02d580af06
Yves Tscherry <info@feerglas.com>
2021-10-14 15:17:55 +0200

    fix: only define default options in dev mode

--- src/preset/addParameter.ts
+++ src/preset/addParameter.ts
@@ -1,19 +1,36 @@
+/*
+Example options:
+
+breakpointNames: {
+  debounceTimeout: 200,
+  nameMap: {
+    'small': '500',
+    'large': '1500',
+    'medium': '1000'
+  }
+}
+*/
+
 import { config } from '../breakpoints';
 
 const parameters: any = {};
 
-// create main options object
-parameters[config.optionKeys.main] = {};
+if (process.env.NODE_ENV === 'development') {
 
-// set breakpoint names option
-parameters[config.optionKeys.main][config.optionKeys.breakpointNames] = {
-  'small': '500',
-  'large': '1500',
-  'medium': '1000'
-};
+  // create main options object
+  parameters[config.optionKeys.main] = {};
 
-// set debounce option
-parameters[config.optionKeys.main][config.optionKeys.debounce] = 200;
+  // set breakpoint names option
+  parameters[config.optionKeys.main][config.optionKeys.breakpointNames] = {
+    'small': '500',
+    'large': '1500',
+    'medium': '1000'
+  };
+
+  // set debounce option
+  parameters[config.optionKeys.main][config.optionKeys.debounce] = 200;
+
+}
 
 export {
   parameters
