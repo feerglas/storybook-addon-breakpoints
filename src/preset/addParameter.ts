dcfa2d3fbc22ac8e3f7a9a77e11918a627fc0143
Yves Tscherry <info@feerglas.com>
2021-10-14 15:02:16 +0200

    refactor: use options keys from config

--- src/preset/addParameter.ts
+++ src/preset/addParameter.ts
@@ -1,7 +1,19 @@
-export const parameters = {
-  breakpointNames: {
-    'small': '500',
-    'large': '1500',
-    'medium': '1000'
-  }
+import { config } from '../breakpoints';
+
+const parameters: any = {};
+
+parameters[config.optionKeys.main] = {};
+
+// set breakpoint names option
+parameters[config.optionKeys.main][config.optionKeys.breakpointNames] = {
+  'small': '500',
+  'large': '1500',
+  'medium': '1000'
+};
+
+// set debounce option
+parameters[config.optionKeys.main][config.optionKeys.debounce] = 200;
+
+export {
+  parameters
 };
