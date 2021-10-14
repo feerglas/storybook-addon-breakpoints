2f2450f946dc577d60358c63723c4555175213ae
Yves Tscherry <info@feerglas.com>
2021-10-14 07:50:13 +0200

    chore: remove unneccessary constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,2 @@
 export const ADDON_ID = "storybook/addon-breakpoints";
 export const TOOL_ID = `${ADDON_ID}/tool`;
-
-export const EVENTS = {
-  RESULT: `${ADDON_ID}/result`,
-  REQUEST: `${ADDON_ID}/request`,
-  CLEAR: `${ADDON_ID}/clear`,
-};
