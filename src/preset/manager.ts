ca06728112024b094ed0ca31bdc62a1e54b67831
Yves Tscherry <info@feerglas.com>
2021-10-14 14:06:52 +0200

    fix: restrict addon to docs only

--- src/preset/manager.ts
+++ src/preset/manager.ts
@@ -3,13 +3,11 @@ import { addons, types } from "@storybook/addons";
 import { ADDON_ID, TOOL_ID } from "../constants";
 import { Tool } from "../Tool";
 
-// Register the addon
-addons.register(ADDON_ID, () => {
-  // Register the tool
+addons.register(ADDON_ID, (api) => {
   addons.add(TOOL_ID, {
     type: types.TOOL,
     title: "Breakpoints",
-    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
+    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story)$/)),
     render: Tool,
   });
 
